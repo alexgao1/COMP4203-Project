87a29d9adfb22e74c55690d888311984121c2ec0
Gabriel Jensen <gabriel.jensen11@hotmail.ca>
2020-03-13 15:19:28 -0400

    Added classes for base and sensor nodes - Added global array for all the nodes - Now the base node starts at an initial position and every other node is created from there

--- wsnsimpy/examples/3dma.py
+++ wsnsimpy/examples/3dma.py
@@ -2,12 +2,16 @@ import random
 import sys
 import wsnsimpy.wsnsimpy_tk as wsp
 
-SOURCE = 1
-DEST   = 99
+# SOURCE = 1
+# Changing DEST to 1 and everything else will be a source
+DEST   = 1
 
 NODE_TX_RANGE = 100
 TERRAIN_SIZE = (700,700)
 TERRAIN_SIZE_WITH_Z = (700,700,500)
+
+# Global container for all nodes
+ALL_NODES = []
 
 ###########################################################
 def delay():
@@ -31,7 +35,6 @@ def gen_within_range(pos, range):
             newZ = gen_rand_sphere_point(pos, 2, range)
         if is_point_in_sphere(pos, range, (newX, newY, newZ)):
             return (newX, newY, newZ)
-
     
 ###########################################################
 class MyNode(wsp.Node):
@@ -117,6 +120,81 @@ class MyNode(wsp.Node):
                 self.log(f"Got data from {src} with seq {seq}")
 
 ###########################################################
+class base_node(wsp.Node):
+
+    ###################
+    # Is only destination
+    def run(self):
+        # if self.id is SOURCE:
+        #     self.scene.nodecolor(self.id,0,0,1)
+        #     self.scene.nodewidth(self.id,2)
+        #     yield self.timeout(1)
+        #     self.send_rreq(self.id)
+        # elif self.id is DEST:
+        self.scene.nodecolor(self.id,1,0,0)
+        self.scene.nodewidth(self.id,2)
+        # else:
+        #     self.scene.nodecolor(self.id,.7,.7,.7)
+
+    ###################
+    def on_receive(self, sender, msg, src, **kwargs):
+        # When receiving data log it
+        if msg == 'data':
+            seq = kwargs['seq']
+            self.log(f"Got data from {src} with seq {seq}")
+
+
+###########################################################
+class sensor_node(wsp.Node):
+
+    ###################
+    def init(self):
+        super().init()
+        self.prev = None
+
+    ###################
+    def run(self):
+        # I do not know which color should be picked
+        # if self.id is SOURCE:
+        #     self.scene.nodecolor(self.id,0,0,1)
+        #     self.scene.nodewidth(self.id,2)
+        #     yield self.timeout(1)
+        #     self.send_rreq(self.id)
+        # elif self.id is DEST:
+        #     self.scene.nodecolor(self.id,1,0,0)
+        #     self.scene.nodewidth(self.id,2)
+        # else:
+        self.scene.nodecolor(self.id,.7,.7,.7)
+
+    ###################
+    # 3DMA routing protocol
+
+    ###################
+    def start_send_data(self):
+        self.scene.clearlinks()
+        seq = 0
+        while True:
+            yield self.timeout(1)
+            self.log(f"Send data to {DEST} with seq {seq}")
+            self.send_data(self.id, seq)
+            seq += 1
+
+    ###################
+    def send_data(self,src,seq):
+        self.log(f"Forward data with seq {seq} via {self.next}")
+        self.send(self.next, msg='data', src=src, seq=seq)
+
+    ###################
+    def on_receive(self, sender, msg, src, **kwargs):
+        if msg == 'data':
+            if self.id is not DEST:
+                yield self.timeout(.2)
+                self.send_data(src,**kwargs)
+            else:
+                seq = kwargs['seq']
+                self.log(f"Got data from {src} with seq {seq}")
+
+###########################################################
 sim = wsp.Simulator(
         until=100,
         timescale=1,
@@ -128,18 +206,20 @@ sim = wsp.Simulator(
 sim.scene.linestyle("parent", color=(0,.8,0), arrow="tail", width=2)
 
 # place nodes over 100x100 grids
-# baseNode = sim.add_node(MyNode, (5,5,5))
-# baseNode.tx_range = NODE_TX_RANGE
-# baseNode.logging = True
+# base_node = sim.add_node(MyNode, (5,5,5))
+# base_node.tx_range = NODE_TX_RANGE
+# base_node.logging = True
 
-prevCoords = (100 + random.uniform(-40,40),
-              100 + random.uniform(-40,40),
-              100 + random.uniform(-40,40))       
-for numNodes in range(0, 125):
-    node = sim.add_node(MyNode, prevCoords)
+prevCoords = (40, 40, 40)
+base_node = sim.add_node(base_node, prevCoords)
+base_node.logging = True
+ALL_NODES.append(base_node)       
+for numNodes in range(1, 126):
+    prevCoords = gen_within_range(prevCoords, NODE_TX_RANGE)
+    node = sim.add_node(sensor_node, prevCoords)
     node.tx_range = NODE_TX_RANGE
     node.logging = True
-    prevCoords = gen_within_range(prevCoords, NODE_TX_RANGE)
+    ALL_NODES.append(node)
     
 # start the simulation
 sim.run()
